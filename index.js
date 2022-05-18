af650cead930e52dee3cf7552dd977d5324eaacc
eejdan <65033345+eejdan@users.noreply.github.com>
2022-05-18 12:24:19 +0300

    initial template

---
+++ index.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+const express = require('express');
+
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(80);
