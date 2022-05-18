8af126561b35395238371602fefdf1535b669f95
Costin <ghenea.costin@gmail.com>
2022-05-18 16:30:26 +0300

    Main Page

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const express = require('express');
 
 
 const app = express();
+const ejs = require("ejs");
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+
 
 
 app.set('view engine', 'ejs');
@@ -14,5 +18,22 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.render('index')
 })
+app.get('/profil', (req, res) =>
+ {     res.render('profil') })
+
+ app.get('/practica', (req, res) =>
+ {     res.render('practica') })
+
+ app.get('/setari', (req, res) =>
+ {     res.render('setari') })
+
+ app.get('/firme', (req, res) =>
+ {     res.render('firme') })
+
+ app.get('/cv', (req, res) =>
+ {     res.render('cv') })
+     
+
+
 
 app.listen(80, () => console.log('server: alive'));
